0bc9744d4bf1fd814885ad50b6d05da51f571d10
Joe Lee <19joelee@gmail.com>
2022-10-13 10:12:47 -0500

    Initial

--- Day6.js
+++ Day6.js
@@ -7,19 +7,35 @@ function syncReadFile(filename) {
 
   var testData = arr[0].split(',');
 
-  for (var d = 0; d < 80; d++){
-    for (var f = 0; f < testData.length; f++){
-        if (testData[f] == 0){
-            testData[f] = 6;
-            testData.concat('8');
-        }
-        else {
-            testData[f]--;
-        }
+  function Calculate(arr) {
+    let temp = [...arr];
+    for (var d = 0; d < 256; d++){
+      let additions = [];
+      for (var f = 0; f < temp.length; f++){
+          if (temp[f] == 0){
+            temp[f] = 6;
+            additions.push(8);
+          }
+          else {
+            temp[f]--;
+          }
+      }
+      if (additions.length > 0){
+        console.log(additions.length);
+        temp = temp.concat(additions);
+      }
     }
-}
+    return temp.length;
+  }
 
-  return testData;
+  var count = 0;
+  var slice = testData.length / 100;
+  for (var i = 0; i < 10; i++){
+    var temp = testData.slice(i * slice, (i * slice) + slice);
+    count += Calculate(temp);
+  }  
+
+  return count;
 }
 
 console.log(syncReadFile('./Day6Data.txt'));
