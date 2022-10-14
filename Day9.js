4b2b0400d3358ab82161956bb7fe0956caa6f1aa
Joe Lee <19joelee@gmail.com>
2022-10-14 08:44:58 -0500

    Completed part 1 of Day9

--- Day9.js
+++ Day9.js
@@ -5,19 +5,65 @@ function syncReadFile(filename) {
 
   const arr = contents.split(/\r?\n/);
 
-  let top = [];
-  let current = [];
-  let bottom = [];
-
+  let sum = 0;
+  
   for (var i = 0; i < arr.length; i++){
     if (i === 0){
-        currentLine
+      sum += firstAndLastLine(arr[i].split(""), arr[i + 1].split(""));
+    }
+    else if (i === arr.length - 1){
+      sum += firstAndLastLine(arr[i].split(""), arr[i - 1].split(""));
+    }
+    else {
+      sum += regularLine(arr[i - 1].split(""), arr[i].split(""), arr[i + 1].split(""));
     }
   }
 
-  function firstLine()
+  function firstAndLastLine(current, other){
+    let sum = 0;
+    for (var i = 0; i < current.length; i++){
+      if (i === 0){
+        if ((Number(current[i]) < Number(current[i + 1])) && (Number(current[i]) < Number(other[i]))){
+          sum += Number(current[i]) + 1;
+        }
+      }
+      else if (i === current.length - 1){
+        if ((Number(current[i]) < Number(current[i - 1])) && (Number(current[i]) < Number(other[i]))){
+          sum += Number(current[i]) + 1;
+        }
+      }
+      else{
+        if ((Number(current[i]) < Number(current[i - 1])) && (Number(current[i]) < Number(current[i + 1])) && (Number(current[i]) < Number(other[i]))){
+          sum += Number(current[i]) + 1;
+        }
+      }
+    }
+    return sum;
+  }
 
-  return 0;
+  function regularLine(top, middle, bottom){
+    let sum = 0;
+    for (var i = 0; i < middle.length; i++){
+      if (i === 0){
+        if ((Number(middle[i]) < Number(middle[i + 1])) && (Number(middle[i]) < Number(top[i])) && (Number(middle[i]) < Number(bottom[i]))){
+          sum += Number(middle[i]) + 1;
+        }
+      }
+      else if (i === middle.length - 1){
+        if ((Number(middle[i]) < Number(middle[i - 1])) && (Number(middle[i]) < Number(top[i])) && (Number(middle[i]) < Number(bottom[i]))){
+          sum += Number(middle[i]) + 1;
+        }
+      }
+      else {
+        if ((Number(middle[i]) < Number(middle[i + 1])) && (Number(middle[i]) < Number(middle[i - 1])) && (Number(middle[i]) < Number(top[i])) && (Number(middle[i]) < Number(bottom[i]))){
+          sum += Number(middle[i]) + 1;
+        }
+      }
+    }
+    return sum;
+  }
+
+  return sum;
 }
 
 console.log(syncReadFile('./Day9Data.txt'));
